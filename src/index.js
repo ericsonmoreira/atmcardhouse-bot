13edf76203208c95824d4d433d7e17262ca180ad
Ericson Moreira <moreira.ericson@gmail.com>
2023-01-18 13:43:25 -0300

    ✨ bot funcionando

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Testando");
+import "dotenv/config";
+import { Telegraf } from "telegraf";
+
+const telegramToken = process.env.BOT_TELEGRAM_TOKEN;
+
+const telegramBot = new Telegraf(telegramToken);
+
+telegramBot.start((ctx) => {
+  ctx.reply("🔥🔥🔥 Seja bem vindo 🔥🔥🔥");
+});
+
+await telegramBot.launch();
+
+// Finalizando processos do Telegram
+process.once("SIGINT", () => telegramBot.stop("SIGINT"));
+process.once("SIGTERM", () => telegramBot.stop("SIGTERM"));
